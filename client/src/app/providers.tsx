6ee70d3caf6b97c22c9152707d28401a3f8d7c77
AshimChoudhary <panghalashim7832@gmail.com>
2025-09-09 23:47:17 +0530

    AWS

--- client/src/app/providers.tsx
+++ client/src/app/providers.tsx
@@ -2,14 +2,11 @@
 
 import StoreProvider from '@/state/redux';
 import { Authenticator } from '@aws-amplify/ui-react';
-import Auth from './(auth)/authProvider';
 
 const Providers = ({ children }: { children: React.ReactNode }) => {
   return (
     <StoreProvider>
-      <Authenticator.Provider>
-        <Auth>{children}</Auth>
-      </Authenticator.Provider>
+      <Authenticator.Provider> {children}</Authenticator.Provider>
     </StoreProvider>
   );
 };
