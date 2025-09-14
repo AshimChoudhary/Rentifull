13de98cba615d56b4a34ab53fd3d84537a448f2a
AshimChoudhary <panghalashim7832@gmail.com>
2025-09-14 23:25:41 +0530

    Redux

--- client/src/state/api.ts
+++ client/src/state/api.ts
@@ -1,12 +1,26 @@
-import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+import { fetchAuthSession, getCurrentUser } from 'aws-amplify/auth';
 
 export const api = createApi({
   baseQuery: fetchBaseQuery({
     baseUrl: process.env.NEXT_PUBLIC_API_BASE_URL,
   }),
-  reducerPath: "api",
+  reducerPath: 'api',
   tagTypes: [],
-  endpoints: (build) => ({}),
+  endpoints: (build) => ({
+    getAuthUser: build.query<User, void>({
+      queryFn: async (_, queryApi, _extraoptions, fetchWithBQ) => {
+        try {
+          const session = await fetchAuthSession();
+          const { idToken } = session.tokens ?? {};
+          const user = await getCurrentUser();
+          const userRole = idToken?.payload['custom:role'] as string;
+
+          const endpont = 
+        } catch (error) {}
+      },
+    }),
+  }),
 });
 
 export const {} = api;
