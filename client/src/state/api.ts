66cf2a3f908a482b82bfd7b16f821a88c7682e5f
AshimChoudhary <panghalashim7832@gmail.com>
2025-10-17 18:59:09 +0530

    User Roles

--- client/src/state/api.ts
+++ client/src/state/api.ts
@@ -1,15 +1,24 @@
+import { Manager, Tenant } from '@/types/prismaTypes';
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 import { fetchAuthSession, getCurrentUser } from 'aws-amplify/auth';
 
 export const api = createApi({
   baseQuery: fetchBaseQuery({
     baseUrl: process.env.NEXT_PUBLIC_API_BASE_URL,
+    prepareHeaders: async (headers) => {
+      const session = await fetchAuthSession();
+      const { idToken } = session.tokens ?? {};
+      if (idToken) {
+        headers.set('Authorization', `Bearer ${idToken}`);
+      }
+      return headers;
+    },
   }),
   reducerPath: 'api',
   tagTypes: [],
   endpoints: (build) => ({
     getAuthUser: build.query<User, void>({
-      queryFn: async (_, queryApi, _extraoptions, fetchWithBQ) => {
+      queryFn: async (_, _queryApi, _extraoptions, fetchWithBQ) => {
         try {
           const session = await fetchAuthSession();
           const { idToken } = session.tokens ?? {};
@@ -21,8 +30,20 @@ export const api = createApi({
               ? `/managers/${user.userId}`
               : `/tenants/${user.userId}`;
 
-          let userDetailsResponse = await fetchWithBQ;
-        } catch (error) {}
+          let userDetailsResponse = await fetchWithBQ(endpoint);
+
+          // If user doesn't exist, create new user
+
+          return {
+            data: {
+              cognitoInfo: { ...user },
+              userInfo: userDetailsResponse.data as Tenant | Manager,
+              userRole,
+            },
+          };
+        } catch (error: any) {
+          return { error: error.message || 'Could not fetch user details' };
+        }
       },
     }),
   }),
